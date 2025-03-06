d168df8bc302081992c16cadf448368d3f8fbfe9
weviavek <we.viavek@gmail.com>
2025-03-06 23:09:35 +0530

    dio exception handling

--- lib/data/services/post_services.dart
+++ lib/data/services/post_services.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:pexa/common/constants/api_constants.dart';
 import 'package:pexa/common/models/api_response.dart';
@@ -6,10 +8,16 @@ import 'package:pexa/data/models/model_post.dart';
 class PostServices {
   final Dio _dio = Dio();
   Future<ApiResponse<List<ModelPost>>> getPosts() async {
-    final response = await _dio.get(ApiConstants.posts);
-    return ApiResponse(
-      success: response.statusCode == 200,
-      data: List.generate(response.data?.length ?? 0, (index) => ModelPost.fromJson(response.data[index])),
-    );
+    try {
+      final response = await _dio.get(ApiConstants.posts);
+      return ApiResponse(
+        success: response.statusCode == 200,
+        data: List.generate(response.data?.length ?? 0, (index) => ModelPost.fromJson(response.data[index])),
+      );
+    } on DioException {
+      return ApiResponse(success: false, message: 'Dio exception');
+    } catch (e) {
+      return ApiResponse(success: false);
+    }
   }
 }
